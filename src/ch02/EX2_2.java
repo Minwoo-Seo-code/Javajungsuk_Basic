1d5fec93188bc230526ebbda1cc9bc36a15d0153
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-12-01 23:55:14 +0900

    [변수와 상수]

---
+++ src/ch02/EX2_2.java
@@ -0,0 +1,15 @@
+package ch02;
+
+public class EX2_2 {
+
+	public static void main(String[] args) {
+		
+		int score1 = 100;
+		score1 = 200;
+		System.out.printf("%d\n", score1);
+		
+		final int score2 = 100;
+//		score2 = 200;
+		System.out.printf("%d", score2);
+	}
+}
