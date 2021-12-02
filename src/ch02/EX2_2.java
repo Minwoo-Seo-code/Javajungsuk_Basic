c92157659ba36c0ea57b53f452b2b352e5f224f5
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-12-02 23:11:21 +0900

    [Scanner Class]

--- src/ch02/EX2_2.java
+++ src/ch02/EX2_2.java
@@ -1,15 +1,21 @@
 package ch02;
 
+import java.util.Scanner;
+
 public class EX2_2 {
 
 	public static void main(String[] args) {
 		
-		int score1 = 100;
-		score1 = 200;
-		System.out.printf("%d\n", score1);
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int num2 = sc.nextInt();
+		System.out.println(num);
+		System.out.println(num2);
 		
-		final int score2 = 100;
-//		score2 = 200;
-		System.out.printf("%d", score2);
+		String first = num + "";
+		System.out.println("first : " + first);
+		
+		int chNum = Integer.parseInt(first);
+		System.out.println("chNum : " + chNum);
 	}
 }
