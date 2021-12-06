52139075d4f3b0ff9291b3e7f3579e78f2878649
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-12-06 23:45:35 +0900

    [OOP example1]

---
+++ src/ch06/LGtv.java
@@ -0,0 +1,15 @@
+package ch06;
+
+public class LGtv {
+
+	public static void main(String[] args) {
+
+		TV lg = new TV();
+		
+		lg.turnOn();
+		lg.volumeUp();
+		lg.volumeDown();
+		lg.turnOff();
+	}
+
+}
---
+++ src/ch06/TV.java
@@ -0,0 +1,18 @@
+package ch06;
+
+public class TV {
+
+	public void turnOn() {
+		System.out.println("전원이 켜졌습니다.");
+	}
+	public void turnOff() {
+		System.out.println("전원이 꺼졌습니다.");
+	}
+	public void volumeUp() {
+		System.out.println("소리가 작아졌습니다.");
+	}
+	public void volumeDown() {
+		System.out.println("소리가 커졌습니다.");
+	}
+	
+}
