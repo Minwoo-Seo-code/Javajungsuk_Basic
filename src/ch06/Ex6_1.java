11f2ff42895a793e27a96a904b55d93622d5ab0e
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-12-06 23:27:49 +0900

    [Object Oriented Programming]

---
+++ src/ch06/Ex6_1.java
@@ -0,0 +1,19 @@
+package ch06;
+
+public class Ex6_1 {
+
+	public static void main(String[] args) {
+
+		//객체지향 언어 Object Oriented Programming (OOP)
+		/*
+		 * 객체 : 현실세계의 것을 코드로 나타내는 것
+		 * 장점 : 재사용성이 높고, 유지보수가 용이, 중복 코드를 제거한다.
+		 * 
+		 * 객체 지향 언어의 특징 
+		 * 캡상추다 (캡슐화, 상속, 추상화, 다형성)
+		 * 
+		 * 클래스 : 객체를 정의해 놓은 것
+		 * 
+		 * */
+	}
+}
