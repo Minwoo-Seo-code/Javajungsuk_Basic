c1ab7cd4a5359a2be7f6ae08552bce1a64aef7ce
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-12-07 23:25:08 +0900

    [변수 종류]

---
+++ src/ch06/Ex6_4.java
@@ -0,0 +1,26 @@
+package ch06;
+
+public class Ex6_4 {
+
+	public static void main(String[] args) {
+		
+		//int iv;
+		//static int cv;
+		
+//		void method() {
+//			int lv = 0;
+//		}
+		 
+		//선언 위치에 따른 변수의 종류
+		/*
+		 * 메소드 안에서 변수가 정의되면 *지역변수라고 부르며, 메서드 영역 안에서만 사용할 수 있다.
+		 * 클래스 안에서 정의가 되면 인스턴스 변수, static하면 클래스 변수, static변수 라고 한다.
+		 * 
+		 * 인스턴스 변수는 인스턴스가 생성되었을 때 생성이 되고,
+		 * static 변수는 클래스가 메모리에 올라갈 때 생성이 된다.
+		 * 
+		 * */
+		
+	}
+
+}
