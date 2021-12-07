30ba75e7709aee06256640499334fe3198057582
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-12-07 22:56:21 +0900

    [사용자 정의 객체]

---
+++ src/ch06/Ex6_3.java
@@ -0,0 +1,27 @@
+package ch06;
+
+public class Ex6_3 {
+
+	public static void main(String[] args) {
+		
+		//클래스의 개념
+		
+		/*
+		 * 클래스란 1. 설계도 이면서
+		 * 		  2. 데이터와 함수의 모음이고,
+		 * 		  3. 사용자가 정의한 타입이라고 설명할 수 있다.
+		 * */
+		
+		//e.g) 사용자 정의 타입
+		/*
+		 *  아래와 같이 타임 클래스를 정의하면 여러 시간과 관련된 객체를 생성할 수 있다.
+		 * Class Time{
+		 * 		int hour;
+		 * 		int minute;
+		 * 		int second;
+		 * 	}
+		 * */
+		
+	}
+
+}
