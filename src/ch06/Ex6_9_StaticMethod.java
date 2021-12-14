c0e749986a5ab542223ce3c2b119466f6f973511
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-12-14 23:34:02 +0900

    [Static / Instance Method]

---
+++ src/ch06/Ex6_9_StaticMethod.java
@@ -0,0 +1,26 @@
+package ch06;
+
+public class Ex6_9_StaticMethod {
+
+	public static void main(String[] args) {
+		
+		//Static 메소드와 Instance 메소드
+		
+		/*
+		 * 인스턴스 메소드
+		 * 
+		 * 인스턴스 즉, 객체를 생성 후, 참조변수.메소드 이름()으로 호출한다.
+		 * 인스턴스 멤버와 관련된 작업을 하는 메소드
+		 * 
+		 * Static 메소드
+		 * 
+		 * 객체생성없이 클래스이름.메소드이름()으로 호출한다. 
+		 * e.g) Math.round() / Math 클래스 사용시, Math객체를 생성해줄 필요가 없다.
+		 * 
+		 * Static 메소드와 인스턴스 메소드의 차이점은 iv, instance variables
+		 * 즉, 인스턴스 변수의 유무이다.
+		 * 
+		 * */
+	
+	}
+}
