1bda35f6d408e46d5c81bbbb572eb9fd9fc409c2
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-12-16 23:57:12 +0900

    [Two type of this]

---
+++ src/ch06/Ex6_11_This.java
@@ -0,0 +1,20 @@
+package ch06;
+
+public class Ex6_11_This {
+	
+	public static void main(String[] args) {
+	
+	//생성자 this의 역할은 인스턴스 변수의 초기화 역할을 한다.
+	//참조변수 this는 지역변수와 인스턴스 변수를 구분하기 위해 사용한다.
+		
+	/*생성자 this
+	 * 생성자 this는 다른 생성자를 호출하기 위해서 사용한다.
+	 * 
+	 * 참조변수 this
+	 * 
+	 * this.color로 표기하는 이유는 다른 매개변수와 이름만으로 구별하기가 힘들어서 그렇다.
+	 * */
+		
+		
+	}
+}
